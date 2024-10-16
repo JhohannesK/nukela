97fd6108c9fe7c03840cf83e0151770716cf562c
jhohannesK <jdomeh77@gmail.com>
2024-10-16 14:25:26 +0000

    connect application to db

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,14 @@
+import Categories from '@/components/categories';
 import SearchInput from '@/components/search-input';
+import prismadb from '@/lib/prismadb';
 import React from 'react';
 
-const RootPage = () => {
+const RootPage = async () => {
+	const categories = await prismadb.category.findMany();
 	return (
 		<div>
 			<SearchInput />
+			<Categories data={categories} />
 		</div>
 	);
 };
---
+++ components/categories.tsx
@@ -0,0 +1,8 @@
+import { Category } from '@prisma/client';
+import React from 'react';
+
+const Categories = ({ data }: { data: Category[] }) => {
+	return <div></div>;
+};
+
+export default Categories;
